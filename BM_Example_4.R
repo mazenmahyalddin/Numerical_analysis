fce911d9e360afe1b728b296b52dd182bcdbad30
mazenkandur <mazenkandur@gmail.com>
2024-04-20 20:40:44 +0300

    Examples TestBook

---
+++ BM_Example_4.R
@@ -0,0 +1,42 @@
+# Define the function
+
+f = function(x){
+  return(x^3 - 2*x -5)
+}
+
+# Define the interval, tolerance level
+a = 2
+b = 3
+tolerance = 0.0001
+
+# Check if there is a root in the interval
+if (f(a) * f(b) >= 0){
+  print("Bur aralikta kok yoktur")
+}	else{
+  # Count variable will keep track of the current iteration number
+  count = 0 
+  
+  # While loop to make sure we stop when the tolerance level is reached
+  while(abs(a-b) > tolerance){
+    c = ((a+b)/2)
+    
+    # Check if there is a root in the new interval and update the interval
+    if(f(a) * f(c) < 0){
+      b = c
+    }	else{
+      a = c
+    }
+    count = count + 1
+    cat(count,"inci iterasyonda, kok  = ", c, "\n")
+  }
+  cat(count,"iterasyonda islem durdurulur. ve f(",c," )=", f(c))
+}
+
+
+
+
+
+
+
+
+
